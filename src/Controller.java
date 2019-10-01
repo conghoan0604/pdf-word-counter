31f6b241888fc702d5a2f752fdadd56be9a1452d
Cong Hoan Ngo <conghoan.0406@gmail.com>
2019-10-01 23:05:42 +0700

    MVC Architecture

---
+++ src/Controller.java
@@ -0,0 +1,58 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+class Controller {
+    private Model theModel;
+    private View theView;
+
+    Controller(View theView, Model theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+        this.theView.addActionListener(new  btnChooseActionListener(), new btnCountActionListener(), new btnAboutActionListener());
+    }
+
+    public class btnCountActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            theView.setResult(theModel.getResult());
+        }
+    }
+
+    public class btnChooseActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter extFilter = new FileNameExtensionFilter("PDF File", "pdf");
+                chooser.setFileFilter(extFilter);
+
+                int returnVal = chooser.showOpenDialog((JButton) e.getSource());
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    theView.setFile(chooser.getSelectedFile());
+                    theView.setFilePathField(chooser.getSelectedFile().getPath());
+                }
+
+                theModel.runCountWord(theView.getFile());
+                theView.setText(theModel.getOriginalText());
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                theView.displayError("Loi trong qua trinh doc file");
+            }
+        }
+
+    }
+
+    public static class btnAboutActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "Danh sách thành viên \n - Ngô Công Hoan - 16001788 \n - Nguyễn Sơn Tùng - 16001888 \n - Đào Hồng Hà - 16001775", "Thông tin nhóm", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
---
+++ src/Model.java
@@ -0,0 +1,182 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import vn.hus.nlp.tokenizer.VietTokenizer;
+
+class Model {
+    private String originalText;
+    private String tokenizedText;
+    private String resourcePath = "resources/";
+    private String resultPath = "results/";
+    private String stopWordsFile = resourcePath + "StopWords.txt";
+    private String originalTextFile = resultPath + "1.OriginalText.txt";
+    private String tokenizedTextFile = resultPath + "2.TokenizedText.txt";
+    private String cleanedTextFile = resultPath + "3.CleanedText.txt";
+    private int result = 0;
+
+    private String getFileExtension(String filePath) {
+        String extension="";
+
+        int i = filePath.lastIndexOf('.');
+        int p = Math.max(filePath.lastIndexOf('/'), filePath.lastIndexOf('\\'));
+
+        if (i > p) {
+            extension = filePath.substring(i+1);
+        }
+
+        return extension;
+    }
+
+    private boolean isPdf(String filePath) {
+        String extension = getFileExtension(filePath);
+        return extension.equals("pdf");
+    }
+
+    private String loadPdf(File file) throws IOException {
+        String filepath = file.getAbsolutePath();
+        boolean isPdf = isPdf(filepath);
+
+        PDDocument document;
+
+        if (file.exists() && isPdf) {
+            document = PDDocument.load(file);
+        } else {
+            document = null;
+            System.exit(0);
+        }
+
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        String documentText = pdfStripper.getText(document);
+        document.close();
+
+        return documentText;
+    }
+
+    private HashSet<String> loadStopWords() {
+        HashSet<String> stopWords = new HashSet<>();
+        BufferedReader bufferedReader = null;
+
+        try {
+            String strCurrentLine;
+            bufferedReader = new BufferedReader(new FileReader(stopWordsFile));
+            while ((strCurrentLine = bufferedReader.readLine()) != null) {
+                stopWords.add(strCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return stopWords;
+    }
+
+    private void writeToFile(String filename, String text) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write (text);
+            writer.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadTxt() {
+        StringBuilder text = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            String currentLine;
+            bufferedReader = new BufferedReader(new FileReader(tokenizedTextFile));
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                text.append(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return text.toString();
+    }
+
+    private static ArrayList<String> convertTextToWordList(String string) {
+        String[] wordArray = string.split(" ");
+        ArrayList<String> wordList = new ArrayList<>();
+
+        Collections.addAll(wordList, wordArray);
+
+        return wordList;
+    }
+
+    private int countWord(ArrayList<String> wordList, HashSet<String> stopWords) {
+        int count = 0;
+
+        for (String word : wordList) {
+            if (!stopWords.contains(word))
+                count++;
+            else {
+                System.out.println(word);
+            }
+        }
+
+        return count;
+    }
+
+    private String cleanString(String string) {
+        return string.replaceAll("[0-9!-/\\[-`:-@{-~]", " ").replaceAll("\\z", " ").replaceAll("\\s\\s+", " ").toLowerCase();
+//        return string.replaceAll("[\r\n|\r|\n0-9$&+,:;=?@#|'<>.^*()%!-/\\\\{}\\[\\]`~]", " ").replaceAll("\\s\\s+", " ").toLowerCase();
+    }
+
+
+    void runCountWord(File file) throws IOException {
+        originalText = loadPdf(file);
+        writeToFile(originalTextFile, originalText);
+
+        VietTokenizer tokenizer = new VietTokenizer();
+        tokenizer.tokenize(originalTextFile, tokenizedTextFile);
+
+        String tokenizedText = loadTxt();
+        ArrayList<String> wordList = convertTextToWordList(tokenizedText);
+
+        String cleanedText = cleanString(tokenizedText);
+        writeToFile(cleanedTextFile, cleanedText);
+
+        HashSet<String> stopWords = loadStopWords();
+
+        result = countWord(wordList, stopWords);
+    }
+
+    String getOriginalText() {
+        return originalText;
+    }
+
+    String getTokenizedText() {
+        return tokenizedText;
+    }
+
+    int getResult() {
+        return result;
+    }
+}
